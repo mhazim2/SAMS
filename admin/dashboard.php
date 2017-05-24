126612d80e4e060fe5587f35d1151e6bb6db22cf
Noer21 <noer.herlambang@yahoo.com>
2017-05-24 13:47:52 +0700

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -10,6 +10,7 @@
  }
  $dataagenda=mysqli_query($conn, "SELECT * FROM agenda");
  $datauser=mysqli_query($conn, "SELECT * FROM users");
+ $datanotif=mysqli_query($conn, "SELECT * FROM users WHERE cek='0' ");
  $datatask=mysqli_query($conn, "SELECT * FROM tasks");
  $dataadmin=mysqli_query($conn, "SELECT * FROM admin");
  $dataadminrow=mysqli_fetch_array($dataadmin);
@@ -105,27 +106,6 @@
                     <a class="navbar-brand" href="#">Dashboard</a>
                 </div>
                 <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav navbar-left">
-                        <li class="dropdown">
-                              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <i class="fa fa-globe"></i>
-                                    <b class="caret hidden-sm hidden-xs"></b>
-                                    <span class="notification hidden-sm hidden-xs">5</span>
-									<p class="hidden-lg hidden-md">
-										5 Notifications
-										<b class="caret"></b>
-									</p>
-                              </a>
-                              <ul class="dropdown-menu">
-                                <li><a href="#">Notification 1</a></li>
-                                <li><a href="#">Notification 2</a></li>
-                                <li><a href="#">Notification 3</a></li>
-                                <li><a href="#">Notification 4</a></li>
-                                <li><a href="#">Another notification</a></li>
-                              </ul>
-                        </li>
-                    </ul>
-
                     <ul class="nav navbar-nav navbar-right">
                         <li class="dropdown">
                               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
@@ -142,9 +122,9 @@
                               </ul>
                         </li>
                         <li>
-                            <a href="#">
-                                <p>Log out</p>
-                            </a>
+                          <a href="logout.php?logout">
+                              <p>Log out</p>
+                          </a>
                         </li>
 						<li class="separator hidden-lg hidden-md"></li>
                     </ul>
